637214bd2800ce648811185adf669938bf3f74cb
Jeff Kelly <9146472+jekelly@users.noreply.github.com>
2022-06-18 14:25:12 -0700

    Factor out necessities to repro breakpoint slowdown

--- d2runner/DiabloEvents.cs
+++ d2runner/DiabloEvents.cs
@@ -1,46 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Reactive.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Forms;
 using System.Windows.Input;
 using Gma.System.MouseKeyHook;
 
 namespace d2runner
 {
-    class Foo : IKeyboardMouseEvents
-    {
-        public event System.Windows.Forms.KeyEventHandler KeyDown;
-        public event KeyPressEventHandler KeyPress;
-        public event EventHandler<KeyDownTxtEventArgs> KeyDownTxt;
-        public event System.Windows.Forms.KeyEventHandler KeyUp;
-        public event System.Windows.Forms.MouseEventHandler MouseMove;
-        public event EventHandler<MouseEventExtArgs> MouseMoveExt;
-        public event System.Windows.Forms.MouseEventHandler MouseClick;
-        public event System.Windows.Forms.MouseEventHandler MouseDown;
-        public event EventHandler<MouseEventExtArgs> MouseDownExt;
-        public event System.Windows.Forms.MouseEventHandler MouseUp;
-        public event EventHandler<MouseEventExtArgs> MouseUpExt;
-        public event System.Windows.Forms.MouseEventHandler MouseWheel;
-        public event System.Windows.Forms.MouseEventHandler MouseHWheel;
-        public event EventHandler<MouseEventExtArgs> MouseWheelExt;
-        public event EventHandler<MouseEventExtArgs> MouseHWheelExt;
-        public event System.Windows.Forms.MouseEventHandler MouseDoubleClick;
-        public event System.Windows.Forms.MouseEventHandler MouseDragStarted;
-        public event EventHandler<MouseEventExtArgs> MouseDragStartedExt;
-        public event System.Windows.Forms.MouseEventHandler MouseDragFinished;
-        public event EventHandler<MouseEventExtArgs> MouseDragFinishedExt;
-
-        public void Dispose()
-        {
-        }
-    }
-
-
     public static class DiabloEvents
     {
         private static IKeyboardMouseEvents evts;
@@ -54,7 +20,7 @@ namespace d2runner
         static DiabloEvents()
         {
             evts = Hook.GlobalEvents();
-            //evts = new Foo();
+            //evts = new DummyKeyboardMouseEvents();
 
             // start a run
             HellGameStarted = evts.GetClickPosition().Buffer(2, 1).Where(x => IsPlayButton(x[0]) && IsHellButton(x[1])).Select(x => DateTimeOffset.Now);
---
+++ d2runner/DummyKeyboardMouseEvents.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+using Gma.System.MouseKeyHook;
+
+namespace d2runner
+{
+    // Dummy IKeyboardMouseEvents to reproduce breakpoing slowdown when hooking input events
+    class DummyKeyboardMouseEvents : IKeyboardMouseEvents
+    {
+        public event System.Windows.Forms.KeyEventHandler KeyDown;
+        public event KeyPressEventHandler KeyPress;
+        public event EventHandler<KeyDownTxtEventArgs> KeyDownTxt;
+        public event System.Windows.Forms.KeyEventHandler KeyUp;
+        public event System.Windows.Forms.MouseEventHandler MouseMove;
+        public event EventHandler<MouseEventExtArgs> MouseMoveExt;
+        public event System.Windows.Forms.MouseEventHandler MouseClick;
+        public event System.Windows.Forms.MouseEventHandler MouseDown;
+        public event EventHandler<MouseEventExtArgs> MouseDownExt;
+        public event System.Windows.Forms.MouseEventHandler MouseUp;
+        public event EventHandler<MouseEventExtArgs> MouseUpExt;
+        public event System.Windows.Forms.MouseEventHandler MouseWheel;
+        public event System.Windows.Forms.MouseEventHandler MouseHWheel;
+        public event EventHandler<MouseEventExtArgs> MouseWheelExt;
+        public event EventHandler<MouseEventExtArgs> MouseHWheelExt;
+        public event System.Windows.Forms.MouseEventHandler MouseDoubleClick;
+        public event System.Windows.Forms.MouseEventHandler MouseDragStarted;
+        public event EventHandler<MouseEventExtArgs> MouseDragStartedExt;
+        public event System.Windows.Forms.MouseEventHandler MouseDragFinished;
+        public event EventHandler<MouseEventExtArgs> MouseDragFinishedExt;
+
+        public void Dispose()
+        {
+        }
+    }
+}
